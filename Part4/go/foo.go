ddbf164bdd4ef126678b6d5b90dacd244cd2fc85
Vebjørn Steinsholt <woodbear92@gmail.com>
2019-01-21 14:14:56 +0100

    Implemented threads in Golang

--- Part4/go/foo.go
+++ Part4/go/foo.go
@@ -25,11 +25,11 @@ func main() {
     runtime.GOMAXPROCS(runtime.NumCPU())    // I guess this is a hint to what GOMAXPROCS does...
 	                                    // Try doing the exercise both with and without it!
 
-    go thread1()
+    go incrementing()
 	
     // We have no way to wait for the completion of a goroutine (without additional syncronization of some sort)
     // We'll come back to using channels in Exercise 2. For now: Sleep.
-    go thread2()
+    go decrementing()
     time.Sleep(100*time.Millisecond)
     Println("The magic number is:", i)
 }
