069613de948d340d231131d46fa297366e17c972
Vebjørn Steinsholt <woodbear92@gmail.com>
2019-01-21 14:13:43 +0100

    Added result for thread in c

--- Part4/go/foo.go
+++ Part4/go/foo.go
@@ -11,21 +11,25 @@ import (
 var i = 0
 
 func incrementing() {
-    //TODO: increment i 1000000 times
+    for j:=0;j<1000000; j++{
+        i++;
+    }
 }
 
 func decrementing() {
-    //TODO: decrement i 1000000 times
-}
+    for j:=0;j<1000000; j++{
+        i--;
+    }}
 
 func main() {
     runtime.GOMAXPROCS(runtime.NumCPU())    // I guess this is a hint to what GOMAXPROCS does...
 	                                    // Try doing the exercise both with and without it!
 
-    // TODO: Spawn both functions as goroutines
+    go thread1()
 	
     // We have no way to wait for the completion of a goroutine (without additional syncronization of some sort)
     // We'll come back to using channels in Exercise 2. For now: Sleep.
+    go thread2()
     time.Sleep(100*time.Millisecond)
     Println("The magic number is:", i)
 }
